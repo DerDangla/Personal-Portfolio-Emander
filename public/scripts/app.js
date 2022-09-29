dbce4f52511534229ba8bca472b4ef81ddc6e536
DerDangla <emanderdangla@gmail.com>
2022-09-29 12:41:53 -0400

    remove unused code in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,20 +23,3 @@
     window.addEventListener("load",Start)
 
 })();
-
-(() => {
-    'use strict'
-  
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })();
